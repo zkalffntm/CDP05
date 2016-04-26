7aead91553653355b1b8f54de9a60478127014a3
zkalffntm <zkalffntm@gmail.com>
2016-04-26 10:51:49 +0900

    서포트 라이브러리 추가

--- MuseumMate/gen/harmony/museummate/R.java
+++ MuseumMate/gen/harmony/museummate/R.java
@@ -22,15 +22,25 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int loading=0x7f020001;
+        public static final int dialog_outter=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int loading_grad=0x7f020002;
+        public static final int loading_text=0x7f020003;
+        public static final int logo=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int imageView1=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int imageView1=0x7f080003;
+        public static final int imageView2=0x7f080004;
+        public static final int imageView3=0x7f080005;
+        public static final int layout_loading=0x7f080002;
+        public static final int mainLayout=0x7f080000;
+        public static final int toolbar=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int dialog=0x7f030001;
+        public static final int loading=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- MuseumMate/src/harmony/museummate/MainActivity.java
+++ MuseumMate/src/harmony/museummate/MainActivity.java
@@ -2,19 +2,52 @@ package harmony.museummate;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends Activity
 {
-
+	private ProgressBar progBar;
+	private Toolbar		toolbar
+	
+	private LoadingHandler handler;
+	
+	private boolean loaded;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.loading);
+		progBar = new ProgressBar(this);
+		RelativeLayout layoutLoading = (RelativeLayout)findViewById(R.id.layout_loading);
+		LayoutParams lParamProgBar = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		lParamProgBar.addRule(RelativeLayout.CENTER_IN_PARENT);
+		layoutLoading.addView(progBar, lParamProgBar);
+		
+		toolbar = (Toolbar) findViewById(R.id.toolbar); //툴바설정
+		toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
+		setSupportActionBar(toolbar);
+		
+		handler = new LoadingHandler();
+		
+		loaded = false;
+		new tools.Loader(handler).start();
 	}
 
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		//progBar.setVisibility(View.VISIBLE);
+	}
+	
+	/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
@@ -22,7 +55,8 @@ public class MainActivity extends Activity
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
@@ -35,5 +69,33 @@ public class MainActivity extends Activity
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	*/
+	
+	class LoadingHandler extends Handler
+	{
+		public static final int
+		SUCCESS = 0,
+		FAILED = -1;
+		
+		@Override
+		public void handleMessage(Message msg)
+		{
+			switch(msg.what)
+			{
+			case SUCCESS:
+				loaded = true;
+				runOnUiThread(new Runnable()
+						{
+							@Override
+							public void run()
+							{ setContentView(R.layout.activity_main); }
+						});
+				break;
+				
+			case FAILED:
+				
+			}
+		}
 	}
 }
